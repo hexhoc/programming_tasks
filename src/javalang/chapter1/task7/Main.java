d1a8e5fe409416fe88fac0ff67ea2a2f3458d983
hexhoc <hexhoc@gmail.com>
2021-07-31 21:40:25 +0300

    task 7 complete

---
+++ src/javalang/chapter1/task7/Main.java
@@ -0,0 +1,13 @@
+package javalang.chapter1.task7;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int intValue = Integer.parseInt("1222333444");
+        long longValue = Long.parseLong("45234223233");
+        float floatValue = Float.parseFloat("45.823F");
+        double doubleValue = Double.parseDouble("13.83423D");
+
+    }
+
+}
