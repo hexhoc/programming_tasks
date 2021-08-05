df47e8b968aa22b894e915e50c50514f69f11b07
hexhoc <hexhoc@gmail.com>
2021-08-05 22:44:41 +0300

    task 21 complete

---
+++ src/chapter1/task21/Main.java
@@ -0,0 +1,8 @@
+package chapter1.task21;
+
+public class Main {
+    public static void main(String[] args) {
+        String result = " hello world ".strip();
+        System.out.println(result);
+    }
+}
