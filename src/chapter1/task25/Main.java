8e9bf7b4b30753603c1ef2f8d6fe4f0a862aefdb
hexhoc <hexhoc@gmail.com>
2021-08-06 08:21:25 +0300

    task 25 complete

---
+++ src/chapter1/task25/Main.java
@@ -0,0 +1,12 @@
+package chapter1.task25;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int x = -14;
+        int y = 50;
+        System.out.println(Math.min(x, y));
+        System.out.println(Math.max(x, y));
+
+    }
+}
