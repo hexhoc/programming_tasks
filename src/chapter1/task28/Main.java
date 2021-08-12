c31efcc7717c6f3f697e03514cadcc42ce660d88
hexhoc <hexhoc@gmail.com>
2021-08-12 22:31:37 +0300

    task 28 complete

---
+++ src/chapter1/task28/Main.java
@@ -0,0 +1,9 @@
+package javalang.chapter1.task28;
+
+public class Main {
+    public static void main(String[] args) {
+        long result = Integer.toUnsignedLong(Integer.MIN_VALUE);
+        int result1 = Short.toUnsignedInt(Short.MIN_VALUE);
+        int result2 = Short.toUnsignedInt(Short.MAX_VALUE);
+    }
+}
