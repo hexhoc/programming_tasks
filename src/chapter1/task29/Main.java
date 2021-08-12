fed16a77238d2501b4570662097e4df59a8e8988
hexhoc <hexhoc@gmail.com>
2021-08-12 22:35:31 +0300

    task 29 complete

---
+++ src/chapter1/task29/Main.java
@@ -0,0 +1,9 @@
+package javalang.chapter1.task29;
+
+public class Main {
+    public static void main(String[] args) {
+        // resultSigned is equal to 1 indicating that
+        // MIN_VALUE is greater than MAX_VALUE
+        int resultUnsigned = Integer.compareUnsigned(Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+}
