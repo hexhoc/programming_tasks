0277ade71d8cd77819a5712d3be23f4d4e08bfa3
hexhoc <hexhoc@gmail.com>
2021-08-13 08:50:36 +0300

    task 35 complete

---
+++ src/chapter1/task35/Main.java
@@ -0,0 +1,10 @@
+package javalang.chapter1.task35;
+
+public class Main {
+    public static void main(String[] args) {
+        double x = 222D / 14;
+        x = Math.floor(x);
+        System.out.println(x);
+
+    }
+}
