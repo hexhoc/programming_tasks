f7fbe3557c99ba79d16e95992438fa349ffd5a0d
hexhoc <hexhoc@gmail.com>
2021-08-17 13:48:03 +0300

    task 41 complete

--- src/chapter2/task41/Main.java
+++ src/chapter2/task41/Main.java
@@ -1,17 +1,19 @@
 package javalang.chapter2.task41;
 
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
 
-        Dog[] arr = new Dog[10];
+        Car[] arr = new Car[10];
 
         for (int i = 0; i < 8; i++) {
-            arr[i] = new Dog();
+            arr[i] = new Car(null);
         }
 
         for (int i = 0; i < 12; i++) {
             try {
-                arr[i].sayHello();
+                arr[i].start();
             } catch (IndexOutOfBoundsException e) {
                 System.out.println("index out of bound");
             } catch (NullPointerException e) {
@@ -22,9 +24,19 @@ public class Main {
         }
     }
 
-    public static class Dog {
-        public void sayHello() {
-            System.out.println("wof wof");
+    public static class Car {
+        private final String name;
+
+        public Car(String name) {
+            //So, if the specified reference is null, then Objects.requireNonNull()
+            //will throw a NullPointerException with the message provided.
+            //Otherwise, it returns the checked reference.
+            this.name = Objects.requireNonNull(name, "Ooops its null");
         }
+
+        public void start() {
+            System.out.println("The car starts");
+        }
+
     }
 }
